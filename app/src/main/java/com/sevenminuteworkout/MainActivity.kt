13770c6979d0149e8199b66be9f4c2a8b4a473fa
AkinchanKushwaha <akinchandroger@gmail.com>
2021-01-21 08:15:37 +0530

    add BMI Calculator.

--- app/src/main/java/com/sevenminuteworkout/MainActivity.kt
+++ app/src/main/java/com/sevenminuteworkout/MainActivity.kt
@@ -2,18 +2,21 @@ package com.sevenminuteworkout
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         llStart.setOnClickListener {
             val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+
+        llBMI.setOnClickListener {
+            // Launching the BMI Activity
+            val intent = Intent(this, BMIActivity::class.java)
             startActivity(intent)
         }
     }
